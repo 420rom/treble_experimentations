7aeedb628c081e445a665003466df9270ddafe18
abun880007 <abun880007@msn.com>
2021-05-08 19:06:27 +0100

    more 420rom bits

--- build.sh
+++ build.sh
@@ -11,41 +11,8 @@ fi
 export LC_ALL=C
 
 manifest_url="https://android.googlesource.com/platform/manifest"
-aosp="android-8.1.0_r65"
-phh="android-8.1"
-
-build_target="$1"
-rebuild_release=""
-if [ "$1" == "android-9.0" ];then
-    manifest_url="https://gitlab.com/aosp-security/manifest"
-    aosp="android-9.0.0_r53-r47"
-    phh="android-9.0"
-elif [ "$1" == "android-10.0" ];then
-    manifest_url="https://android.googlesource.com/platform/manifest"
-    aosp="android-10.0.0_r41"
-    phh="android-10.0"
-elif [ "$1" == "android-11.0" ];then
-    manifest_url="https://android.googlesource.com/platform/manifest"
-    aosp="android-11.0.0_r37"
-    phh="420rom-11"
-else
-	# guess android version from version number
-	rebuild_release="yes"
-	if [ -n "$(echo $1 | grep -E '^v3..')" ];then
-		build_target="android-11.0"
-	elif [ -n "$(echo $1 | grep -E '^v2..')" ];then
-		build_target="android-10.0"
-	elif [ -n "$(echo $1 | grep -E '^v1..')" ];then
-		build_target="android-9.0"
-	else
-		build_target="android-8.1"
-	fi
-	# download manifest with the given version number
-	tmp_manifest_source=$(mktemp -d)
-	wget "https://github.com/420rom/treble_experimentations/releases/download/$1/manifest.xml" -O $tmp_manifest_source/manifest.xml
-	sed -i 's/<remote name="aosp" fetch=".." review="https:\/\/android-review.googlesource.com\/"\/>/<remote name="aosp" fetch="https:\/\/android.googlesource.com\/" review="https:\/\/android-review.googlesource.com\/"\/>/' $tmp_manifest_source/manifest.xml
-	(cd $tmp_manifest_source; git init; git add manifest.xml; git commit -m "$1")
-fi
+aosp="android-11.0.0_r37"
+phh="420rom-11"
 
 if [ "$release" == true ];then
     [ -z "$version" ] && exit 1
@@ -91,6 +58,11 @@ if [ "$build_target" == "android-11.0" ];then
         git clone https://github.com/phhusson/vendor_vndk -b android-10.0
     )
 
+    # ARM64 Gapps {ab, a-only, ab vndk lite}
+	buildVariant treble_arm64_bgS-userdebug roar-arm64-ab-gapps
+    ( cd sas-creator; bash run.sh 64 ; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm64-aonly-gapps.img.xz)
+    ( cd sas-creator; bash lite-adapter.sh 64; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm64-ab-vndklite-gapps.img.xz )
+    
     # ARM64 vanilla {ab, a-only, ab vndk lite}
 	buildVariant treble_arm64_bvS-userdebug roar-arm64-ab-vanilla
     ( cd sas-creator; bash run.sh 64 ; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm64-aonly-vanilla.img.xz)
@@ -101,51 +73,15 @@ if [ "$build_target" == "android-11.0" ];then
     ( cd sas-creator; bash run.sh 64 ; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm64-aonly-floss.img.xz)
     ( cd sas-creator; bash lite-adapter.sh 64; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm64-ab-vndklite-floss.img.xz )
 
-    # ARM32 vanilla {ab, a-only}
-	buildVariant treble_arm_bvS-userdebug roar-arm-ab-vanilla
-    ( cd sas-creator; bash run.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm-aonly-vanilla.img.xz )
 
-    # ARM32 gogapps {ab, a-only}
-	buildVariant treble_arm_boS-userdebug roar-arm-ab-gogapps
-    ( cd sas-creator; bash run.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm-aonly-gogapps.img.xz )
-
-    # ARM32_binder64 vanilla {ab, ab vndk lite}
-	buildVariant treble_a64_bvS-userdebug roar-arm32_binder64-ab-vanilla
-    ( cd sas-creator; bash lite-adapter.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm32_binder64-ab-vndklite-vanilla.img.xz)
-
-    # ARM64 Gapps {ab, a-only, ab vndk lite}
-	buildVariant treble_arm64_bgS-userdebug roar-arm64-ab-gapps
-    ( cd sas-creator; bash run.sh 64 ; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm64-aonly-gapps.img.xz)
-    ( cd sas-creator; bash lite-adapter.sh 64; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm64-ab-vndklite-gapps.img.xz )
-
-    # ARM32_binder64 go gapps {ab, ab vndk lite}
-	buildVariant treble_a64_boS-userdebug roar-arm32_binder64-ab-gogapps
-    ( cd sas-creator; bash lite-adapter.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm32_binder64-ab-vndklite-gogapps.img.xz )
-
-    # ARM32_binder64 gapps {ab, ab vndk lite}
-	buildVariant treble_a64_bgS-userdebug roar-arm32_binder64-ab-gapps
-    ( cd sas-creator; bash lite-adapter.sh 32; xz -c s.img -T0 > ../release/$rom_fp/system-roar-arm32_binder64-ab-vndklite-gapps.img.xz )
 elif [ "$build_target" == "android-10.0" ];then
+	buildVariant treble_arm64_bgS-userdebug quack-arm64-ab-gapps
+	buildVariant treble_arm64_bfS-userdebug quack-arm64-ab-floss
+	buildVariant treble_arm64_bvS-userdebug quack-arm64-ab-vanilla
 	buildVariant treble_arm64_afS-userdebug quack-arm64-aonly-floss
 	buildVariant treble_arm64_avS-userdebug quack-arm64-aonly-vanilla
 	buildVariant treble_arm64_agS-userdebug quack-arm64-aonly-gapps
 	buildVariant treble_arm64_aoS-userdebug quack-arm64-aonly-go
-	buildVariant treble_arm64_bfS-userdebug quack-arm64-ab-floss
-	buildVariant treble_arm64_bvS-userdebug quack-arm64-ab-vanilla
-	buildVariant treble_arm64_bgS-userdebug quack-arm64-ab-gapps
-	buildVariant treble_arm64_boS-userdebug quack-arm64-ab-go
-	buildVariant treble_arm_avS-userdebug quack-arm-aonly-vanilla
-	buildVariant treble_arm_agS-userdebug quack-arm-aonly-gapps
-	buildVariant treble_arm_aoS-userdebug quack-arm-aonly-go
-	buildVariant treble_arm_bvS-userdebug quack-arm-ab-vanilla
-	buildVariant treble_arm_bgS-userdebug quack-arm-ab-gapps
-	buildVariant treble_arm_boS-userdebug quack-arm-ab-go
-	buildVariant treble_a64_avS-userdebug quack-arm32_binder64-aonly-vanilla
-	buildVariant treble_a64_agS-userdebug quack-arm32_binder64-aonly-gapps
-	buildVariant treble_a64_aoS-userdebug quack-arm32_binder64-aonly-go
-	buildVariant treble_a64_bvS-userdebug quack-arm32_binder64-ab-vanilla
-	buildVariant treble_a64_bgS-userdebug quack-arm32_binder64-ab-gapps
-	buildVariant treble_a64_boS-userdebug quack-arm32_binder64-ab-go
 else
 	buildVariant treble_arm64_avN-userdebug arm64-aonly-vanilla-nosu
 	buildVariant treble_arm64_agS-userdebug arm64-aonly-gapps-su
